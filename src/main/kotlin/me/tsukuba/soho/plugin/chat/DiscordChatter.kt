5d4239d175ac2d66f7b6bc5b33da61a7cbbb988e
chihiro sakai <sakaic2003@gmail.com>
2021-09-04 21:14:08 +0900

    create plugin project

---
+++ src/main/kotlin/me/tsukuba/soho/plugin/chat/DiscordChatter.kt
@@ -0,0 +1,13 @@
+package me.tsukuba.soho.plugin.chat
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DiscordChatter: JavaPlugin() {
+    override fun onEnable() {
+        super.onEnable()
+    }
+
+    override fun onDisable() {
+        super.onDisable()
+    }
+}
