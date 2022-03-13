a3a79ce77a41c411ef7116c16e88fd444c87bbf5
frodo821 <sakaic2003@gmail.com>
2022-03-13 21:36:17 +0900

    マインクラフトのコマンドを直接実行しないように修正

--- src/main/kotlin/me/tsukuba/soho/plugin/chat/DiscordChatter.kt
+++ src/main/kotlin/me/tsukuba/soho/plugin/chat/DiscordChatter.kt
@@ -3,15 +3,22 @@ package me.tsukuba.soho.plugin.chat
 import org.bukkit.event.EventHandler
 import io.papermc.paper.event.player.AsyncChatEvent
 import me.tsukuba.soho.plugin.chat.discord.DiscordBot
-import org.bukkit.command.CommandException
+import net.kyori.adventure.text.Component
 import org.bukkit.event.Listener
 import org.bukkit.plugin.PluginLogger
 import org.bukkit.plugin.java.JavaPlugin
+
+typealias CommandHandle = DiscordChatter.(args: List<String>) -> Unit
 
 @Suppress("unused")
 class DiscordChatter: JavaPlugin(), Listener {
     private lateinit var bot: DiscordBot
     private val logger = PluginLogger(this)
+
+    companion object {
+        private const val botId = 883677891844517898L
+        val commandMap = mutableMapOf<String, CommandHandle>()
+    }
 
     override fun onEnable() {
         config.addDefaults(
@@ -25,32 +32,106 @@ class DiscordChatter: JavaPlugin(), Listener {
         val token = config.getString("token") ?: throw IllegalArgumentException("token is null")
         val channelId = config.getLong("channel_id")
 
-        bot = DiscordBot(token, channelId, logger) {
-            val task = object: Runnable {
-                override fun run() {
-                    val sender = DiscordCommandSender(server, this@DiscordChatter)
-                    val cmdline = if (it.startsWith('/')) it.substring(1) else it
+        initializeBotCommands()
 
-                    try {
-                        val executed = server.dispatchCommand(sender, cmdline)
-
-                        if (!executed) {
-                            val name = cmdline.split(' ', limit = 1)[0]
-                            sendMessage("[error] command $name could not be found")
-                            return
-                        }
-                    } catch (err: CommandException) {
-                        sendMessage("[error] unknown error: ${err.message}\n${err.stackTraceToString()}")
-                        return
+        bot = DiscordBot(token, channelId, logger) { it, perm ->
+            val task = Runnable {
+                if (it.memberMentions.any { it.id.asLong() == botId }) {
+                    if (it.author.isEmpty || it.author.get().id.asLong() == botId) {
+                        return@Runnable
                     }
 
-                    sendMessage("[success]\n${sender.getMessage()}")
+                    if (!perm) {
+                        sendMessage("You have no permission to run the command.")
+                        return@Runnable
+                    }
+
+                    val text = it.content
+                        .replace("<@!$botId>", "")
+                        .split(' ', '\n')
+                        .filter { it.isNotEmpty() }
+
+                    if(text.isEmpty()) {
+                        return@Runnable
+                    }
+
+                    val cmd = text.first()
+                    val args = text.drop(1)
+
+                    val cmdHandle = commandMap[cmd] ?: return@Runnable sendMessage("command $cmd can't be found!")
+
+                    this@DiscordChatter.cmdHandle(args)
+
+                    return@Runnable
+                }
+
+                if (it.author.isPresent) {
+                    server.broadcast(Component.text(
+                        "<${it.authorAsMember.block()?.displayName}@discord> ${it.content}"
+                    ))
                 }
             }
             server.scheduler.runTask(this@DiscordChatter, task)
         }
 
         server.pluginManager.registerEvents(this, this)
+    }
+
+    private fun initializeBotCommands() {
+        commandMap["op"] = {
+            if (it.isEmpty()) {
+                bot.sendMessage("Usage: <@!${botId}> op playerName [playerName1 playerName2...]")
+            } else {
+                var message = ""
+                var success = 0
+
+                for (name in it) {
+                    val player = server.getPlayerExact(name)
+
+                    if (player == null) {
+                        message += "player '$name' was not found.\n"
+                        continue
+                    }
+
+                    player.isOp = true
+                    player.recalculatePermissions()
+                    success++
+                }
+
+                bot.sendMessage("${message}successfully oped $success player(s)")
+            }
+        }
+        commandMap["deop"] = {
+            if (it.isEmpty()) {
+                bot.sendMessage("Usage: <@!${botId}> deop playerName [playerName1 playerName2...]")
+            } else {
+                var message = ""
+                var success = 0
+
+                for (name in it) {
+                    val player = server.getPlayerExact(name)
+
+                    if (player == null) {
+                        message += "player '$name' was not found.\n"
+                        continue
+                    }
+
+                    player.isOp = false
+                    player.recalculatePermissions()
+                    success++
+                }
+
+                bot.sendMessage("${message}successfully de-oped $success player(s)")
+            }
+        }
+        commandMap["help"] = {
+            bot.sendMessage("""
+                Minecraft/Discord Chatbot@${description.version} by ${description.authors.joinToString(", ")}
+                
+                ${description.description}
+                
+                Available commands: ${commandMap.keys.joinToString(", ")}""".trimIndent())
+        }
     }
 
     override fun onDisable() {
