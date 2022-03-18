1189b95fbefe063729204daffd00f600ba6617af
frodo821 <sakaic2003@gmail.com>
2022-03-19 07:26:25 +0900

    Discordサーバーからコマンドを実行できるように修正

--- src/main/kotlin/me/tsukuba/soho/plugin/chat/DiscordChatter.kt
+++ src/main/kotlin/me/tsukuba/soho/plugin/chat/DiscordChatter.kt
@@ -2,47 +2,93 @@ package me.tsukuba.soho.plugin.chat
 
 import org.bukkit.event.EventHandler
 import io.papermc.paper.event.player.AsyncChatEvent
+import kotlinx.coroutines.*
+import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.event.player.PlayerKickEvent
 import me.tsukuba.soho.plugin.chat.discord.DiscordBot
+import me.tsukuba.soho.plugin.chat.rcon.RemoteConsoleClient
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.Style
 import net.kyori.adventure.text.format.TextColor
 import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.BanList
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.entity.Player
 import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.server.ServerLoadEvent
 import org.bukkit.plugin.PluginLogger
 import org.bukkit.plugin.java.JavaPlugin
+import java.lang.Runnable
 import java.text.DateFormat
 import java.util.*
+import kotlin.io.path.*
 
 typealias CommandHandle = DiscordChatter.(args: List<String>) -> Unit
 
 @Suppress("unused")
 class DiscordChatter: JavaPlugin(), Listener {
-    private lateinit var bot: DiscordBot
+    private var _bot: DiscordBot? = null
+    private val bot: DiscordBot
+        get() = _bot!!
+    private val initialized get() = _bot != null
     private val logger = PluginLogger(this)
+    private var _nickname: String? = null
+    val nickname: String? get() = _nickname
+    private val props = Properties()
+    private var rcon: RemoteConsoleClient? = null
+
+    init {
+        try {
+            val path = javaClass.protectionDomain.codeSource.location.toURI().toPath()
+
+            val propsPath = (if (path.extension == "jar") {
+                path.parent.parent
+            } else {
+                path.parent.resolve("../".repeat(javaClass.packageName.split('.').size))
+            }).resolve("server.properties")
+
+            if (propsPath.exists() && propsPath.isRegularFile()) {
+                val file = propsPath.toFile()
+                if (file.canRead()) {
+                    file.inputStream().use {
+                        props.load(it)
+                    }
+                    logger.info("server.properties loaded with ${props.size} entries.")
+                } else {
+                    logger.warning("cannot read server.properties, skipping")
+                }
+            } else {
+                logger.warning("server.properties not found at the expected location: $propsPath, skipping.")
+            }
+        } catch (err: java.lang.Exception) {
+            logger.warning("cannot get library path because of security reason: ${err.localizedMessage}")
+        }
+    }
 
     companion object {
         const val botId = 883677891844517898L
         val commandMap = mutableMapOf<String, CommandHandle>()
-        private val dateFormatter = DateFormat.getDateInstance(DateFormat.FULL, Locale.JAPANESE)
+        private val dateFormatter = DateFormat.getDateInstance(DateFormat.FULL, Locale.JAPAN)
     }
 
     override fun onEnable() {
         config.addDefaults(
             mapOf(
                 "token" to null,
-                "channel_id" to null
+                "channel_id" to null,
+                "nickname" to null
             )
         )
         saveConfig()
 
         val token = config.getString("token") ?: throw IllegalArgumentException("token is null")
         val channelId = config.getLong("channel_id")
+        _nickname = config.getString("nickname")
 
         initializeBotCommands()
 
-        bot = DiscordBot(token, channelId, logger) { it, perm ->
+        _bot = DiscordBot(token, channelId, this) { it, perm ->
             val task = Runnable {
                 if (it.memberMentions.any { it.id.asLong() == botId }) {
                     if (it.author.isEmpty || it.author.get().id.asLong() == botId) {
@@ -56,6 +102,7 @@ class DiscordChatter: JavaPlugin(), Listener {
 
                     val text = it.content
                         .replace("<@!$botId>", "")
+                        .replace("<@$botId>", "")
                         .split(' ', '\n')
                         .filter { it.isNotEmpty() }
 
@@ -193,13 +240,17 @@ class DiscordChatter: JavaPlugin(), Listener {
                     }}""".trimMargin("|"))
                 }
                 else -> {
-                    bot.sendMessage("使用方法: <@!${botId}> players <online | offline | banned | all>")
+                    bot.sendMessage("""
+                        使用方法: <@!${botId}> players <online | offline | banned | all>
+                          プレイヤーの数及び一覧を表示します。""".trimIndent())
                 }
             }
         }
         commandMap["ban"] = {
             if (it.isEmpty()) {
-                bot.sendMessage("使用方法: <@!${botId}> ban playerName [reason]")
+                bot.sendMessage("""
+                    使用方法: <@!${botId}> ban playerName [reason]
+                      指定したプレイヤーのアクセスを禁止します。理由は省略可能です。""".trimIndent())
             } else {
                 val name = it.first()
                 val player = server.getPlayerExact(name) ?: server.getOfflinePlayerIfCached(name)
@@ -215,7 +266,9 @@ class DiscordChatter: JavaPlugin(), Listener {
         }
         commandMap["pardon"] = {
             if (it.isEmpty()) {
-                bot.sendMessage("使用方法: <@!${botId}> pardon playerName")
+                bot.sendMessage("""
+                    使用方法: <@!${botId}> pardon playerName
+                      指定したプレイヤーのアクセス禁止を解除します。""".trimIndent())
             } else {
                 val name = it.first()
                 val player = server.getPlayerExact(name) ?: server.getOfflinePlayerIfCached(name)
@@ -228,14 +281,119 @@ class DiscordChatter: JavaPlugin(), Listener {
                 }
             }
         }
+        commandMap["cache"] = {
+            if (it.isEmpty()) {
+                bot.sendMessage("""
+                    使用方法: <@!${botId}> cache <expire | info>
+                      Discordのサーバー情報のキャッシュを確認または更新します。""".trimIndent())
+            } else {
+                when (it.first()) {
+                    "expire" -> runBlocking {
+                        bot.sendMessage("Discordサーバー情報のキャッシュを更新します。")
+                        bot.expireIdCaches()
+                            .doOnSuccess {
+                                bot.sendMessage("Discordサーバー情報のキャッシュを正常に更新しました。")
+                            }
+                            .doOnError {
+                                bot.sendMessage("Discordサーバー情報のキャッシュの更新に失敗しました。")
+                            }
+                            .subscribe()
+                    }
+                    "info" -> {
+                        val info = bot.cacheInfo
+                        if (info.initialized) {
+                            bot.sendMessage("現在、${info.cachedRoles}個のロールと${info.cachedUsers}人" +
+                                            "のユーザー情報がキャッシュされています。" +
+                                            "キャッシュを更新するには、<@!${botId}> cache expire コマンドを利用してください。" +
+                                            "キャッシュが古い場合、ゲーム内からのメンションがうまく動作しないことがあります。")
+                        } else {
+                            bot.sendMessage("初期化が完了していません。")
+                        }
+                    }
+                }
+            }
+        }
+        commandMap["run"] = {
+            if (it.isEmpty()) {
+                bot.sendMessage("""
+                    使用方法: <@!${botId}> run <Minecraft commands>
+                      MinecraftのコマンドをOP権限で実行します。""".trimIndent())
+            } else {
+                val console = rcon
+                if (console == null || !console.isConnected) {
+                    bot.sendMessage("コマンドコンソールが有効になっていないため、コマンドを実行できません")
+                } else {
+                    CoroutineScope(Dispatchers.Default).launch {
+                        val resp = console.command(it.joinToString(" "))
+                        bot.sendMessage(resp.replace(Regex("§\\w"), ""))
+                    }
+                }
+            }
+        }
     }
 
     override fun onDisable() {
-        bot.close()
+        if (initialized) {
+            bot.close()
+            rcon?.close()
+        }
+    }
+
+    private fun connectRcon() = CoroutineScope(Dispatchers.Default).launch {
+        val port = props.getProperty("rcon.port").toIntOrNull()
+        val pass = props.getProperty("rcon.password")
+
+        if (port != null && pass != null) {
+            rcon = run {
+                val con = RemoteConsoleClient(logger, port, pass)
+                if (con.connect()) {
+                    logger.info("rcon connected.")
+                    con
+                } else {
+                    null
+                }
+            }
+        } else {
+            logger.warning("reading config failed")
+        }
     }
 
     @EventHandler
-    @Suppress("unused")
+    fun onServerLoad(event: ServerLoadEvent) {
+        val task = Runnable {
+            logger.info("server loading done, trying to connect remote console from localhost")
+            rcon?.close()
+            connectRcon()
+        }
+
+        server.scheduler.runTaskLaterAsynchronously(this, task, 20)
+    }
+
+    @EventHandler
+    fun onPlayerLoggedIn(event: PlayerJoinEvent) {
+        updateName()
+    }
+
+    @EventHandler
+    fun onPlayerQuit(event: PlayerQuitEvent) {
+        updateName()
+    }
+
+    @EventHandler
+    fun onPlayerKicked(event: PlayerKickEvent) {
+        updateName()
+    }
+
+    private fun updateName() {
+        val task = Runnable {
+            CoroutineScope(Dispatchers.Default).launch {
+                bot.updateNickname("${nickname ?: "mchatter"} [${server.onlinePlayers.size}/${server.maxPlayers}]")
+            }
+        }
+        server.scheduler.runTaskLater(this, task, 10)
+    }
+
+    @EventHandler
     fun onChat(event: AsyncChatEvent) {
         bot.sendMessage(event.player, event.message())
     }
